e6cf30b8e933a22897f5b677db144cf4778876e3
GaganGupta <gagan_gupta111@yahoo.com>
2018-12-02 16:28:01 +0530

    debugger

---
+++ tests.js
@@ -0,0 +1,11 @@
+
+var person = {
+    name : "gagan",
+    age: 29
+}
+
+debugger;
+
+person.name = "gupta";
+
+console.log('person age: ${person.age}');
--- yargs.js
+++ yargs.js
@@ -3,11 +3,37 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
+const titleOptions = {
+    describe: 'Title of Note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of Note',
+    demand: true,
+    alias: 'b'
+};
+
 console.log('regular inputs', process.argv);
 console.log('yargs input', yargs.argv);
 
+
 var arg = process.argv[2];
-var argv = yargs.argv;
+var argv = yargs
+    .command('add', 'Add a new note', {
+        title:titleOptions,
+        body:bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'read a note', {
+        title:titleOptions
+    })
+    .command('remove', 'remove a note', {
+        title:titleOptions
+    })
+    .help()
+    .argv;
 var yargv1 = argv._[0];
 var yargv2 = argv._[1];
 
